e873ca794e57bf6c1733d634f378e314a3c0b37a
Katie Lin <chiachen@ualberta.ca>
2019-08-23 19:35:38 -0600

    Update main.py

--- Application/main.py
+++ Application/main.py
@@ -7,8 +7,10 @@ pygame.init()
 display_height = 1280
 display_width =720
 
+pygame.display.set_caption("deez nuts")
 
 gameDisplay = pygame.display.set_mode((display_height,display_width))
+bg_color = (230, 230, 230)
 
 pygame.font.init()
 font = pygame.font.SysFont('Comic Sans MS', 30)
